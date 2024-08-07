edcd10f9b695271d202eece1d251b00e54b42f28
강민재 <minijae011030@naver.com>
2024-08-07 23:59:37 +0900

    add gender, birth, weight information in healthkit function

--- Page/Login/Function/GetPermissionFunction.js
+++ Page/Login/Function/GetPermissionFunction.js
@@ -6,6 +6,9 @@ export default function GetPermissionFunction() {
         appleHealthKit.Constants.Permissions.SleepAnalysis,
         appleHealthKit.Constants.Permissions.ActivitySummary,
         appleHealthKit.Constants.Permissions.StepCount,
+        appleHealthKit.Constants.Permissions.BiologicalSex,
+        appleHealthKit.Constants.Permissions.DateOfBirth,
+        appleHealthKit.Constants.Permissions.Weight,
       ],
     },
   };
@@ -24,20 +27,31 @@ export default function GetPermissionFunction() {
       endDate: new Date(2024, 7, 30).toISOString(),
     };
 
+    const weightOptions = {
+      unit: 'kg',
+    };
     appleHealthKit.getSleepSamples(options, (error, result) => {
       if (error) {
         console.log('[Error] Failed to load Sleep Data');
+
+        return;
       }
 
       console.log('sleep data', result);
+
+      return;
     });
 
     appleHealthKit.getActivitySummary(options, (error, result) => {
       if (error) {
         console.log('[Error] Failed to load Activity Data');
+
+        return;
       }
 
       console.log('activity data', result);
+
+      return;
     });
 
     appleHealthKit.getDailyStepCountSamples(
@@ -45,10 +59,50 @@ export default function GetPermissionFunction() {
       (error, result) => {
         if (error) {
           console.log('[Error] Failed to load StepCount Data');
+
+          return;
         }
 
-        console.log('stepcount data', result);
+        console.log('step count data', result);
+
+        return;
       },
     );
+
+    appleHealthKit.getBiologicalSex(null, (error, result) => {
+      if (error) {
+        console.log('[Error] Failed to lad Biological Sex');
+
+        return;
+      }
+
+      console.log('biological sex', result);
+
+      return;
+    });
+
+    appleHealthKit.getDateOfBirth(null, (error, result) => {
+      if (error) {
+        console.log('[Error] Failed to load Date of Birth');
+
+        return;
+      }
+
+      console.log('date of birth', result);
+
+      return;
+    });
+
+    appleHealthKit.getLatestWeight(weightOptions, (error, result) => {
+      if (error) {
+        console.log('[Error] Failed to get weight data');
+
+        return;
+      }
+
+      console.log('latest wieght', result);
+
+      return;
+    });
   });
 }
