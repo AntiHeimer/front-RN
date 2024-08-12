5dced1efc6e97a1b951c117168c8d9513d4aa044
강민재 <minijae011030@naver.com>
2024-08-13 02:40:31 +0900

    add Authorization in Logout Function

--- Page/Login/Function/LogoutFunction.js
+++ Page/Login/Function/LogoutFunction.js
@@ -1,13 +1,13 @@
 import EncryptFunction from '../../../Utils/Function/EncryptFunction';
 
-export default async function LogoutFunction({uuid}) {
+export default async function LogoutFunction({uuid, token}) {
   const encryptedUuid = EncryptFunction({data: uuid});
 
   const result = await fetch(`${process.env.API_URL}/logout/${encryptedUuid}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'text/plain',
-      auth: process.env.AUTH_KEY,
+      Authorization: `Bearer ${token}`,
     },
   });
 
