2563129cc9a676dcae2a685f91c8ba3accc47527
강민재 <minijae011030@naver.com>
2024-08-23 15:41:16 +0900

    signup function description

--- Page/SignUp/Function/SignUpFunction.js
+++ Page/SignUp/Function/SignUpFunction.js
@@ -2,6 +2,16 @@ import EncryptFunction from '../../../Utils/Function/EncryptFunction';
 import GetBirthFunction from './GetBirthFunction';
 import GetGenderFunction from './GetGenderFunction';
 
+/**
+ * 사용자가 입력필드로 입력한 정보를 서버로 전송회 회원가입을 수행하는 함수
+ *
+ * @param {Object} props - 컴포넌트에 전달되는 props
+ * @param {string} props.name - 사용자가 입력 필드에 입력한 사용자 이름
+ * @param {string} props.id  - 사용자가 입력 필드에 입력한 아이디
+ * @param {string} props.password - 사용자가 입력 필드에 입력한 비밀번호
+ *
+ * @returns {Promise<void>} - 서버의 응답을 포함하는 Promise
+ */
 export default async function SignUpFunction({name, id, password}) {
   const gender = await GetGenderFunction(); // healthkit에서 성별 불러오기
   const birth = await GetBirthFunction(); // healthkit에서 생년월일 불러오기
