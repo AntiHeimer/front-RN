429585cfd47e160bc3547d4d15007971f3d664da
강민재 <minijae011030@naver.com>
2024-08-27 06:55:11 +0900

    post sleep data function

---
+++ Utils/Function/Health/PostSleepFunction.js
@@ -0,0 +1,31 @@
+import {Storage} from '../Storage';
+import DateFormattingFunction2 from './DateFormattingFunction2';
+import HealthKitService from './HealthkitService';
+
+export default async function PostSleepFunction({startDate, endDate}) {
+  const sleepData = await HealthKitService.getSleepData({startDate, endDate});
+
+  if (sleepData.length == 0) return;
+
+  const formattedSleepData = DateFormattingFunction2(sleepData);
+
+  const userState = await Storage.getItem('userState');
+
+  const uuid = userState.uuid;
+  const token = userState.jwtToken;
+
+  const result = await fetch(`${process.env.API}/save/sleep`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      memberUuid: uuid,
+      date: startDate,
+      sleepData: formattedSleepData,
+    }),
+  });
+
+  const res = await result.json();
+}
