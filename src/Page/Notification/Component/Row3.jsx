0a758607013e771d5d36a0ca8f0f84744c614460
강민재 <minijae011030@naver.com>
2024-09-18 19:42:32 +0900

    commit

--- src/Page/Notification/Component/Row3.jsx
+++ src/Page/Notification/Component/Row3.jsx
@@ -1,5 +1,5 @@
-import {StyleSheet, Text, View} from 'react-native';
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
 import DeleteNotificationFunction from '../Function/DeleteNotificationFunction';
 
 function Row3({notification, navigation}) {
