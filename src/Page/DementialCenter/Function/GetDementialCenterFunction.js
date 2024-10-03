57c08e211c785d220a0b7a1a14b68edf19ddbd5f
강민재 <minijae011030@naver.com>
2024-10-04 02:18:24 +0900

    dementia center

--- src/Page/DementialCenter/Function/GetDementialCenterFunction.js
+++ src/Page/DementialCenter/Function/GetDementialCenterFunction.js
@@ -4,12 +4,15 @@ export default async function GetDementialCenterFunction({page}) {
   const userState = await Storage.getItem('userState');
   const token = userState.jwtToken;
 
-  const result = await fetch(`${process.env.API}/dementia-center?page=1`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`,
+  const result = await fetch(
+    `${process.env.API}/dementia-center?page=${page}`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     },
-  });
+  );
 
   const res = await result.json();
 
